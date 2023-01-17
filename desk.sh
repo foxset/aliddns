a0ec7116f9ec9d6429082cca8a4c70ed7918bfc1
foxset <122256568+foxset@users.noreply.github.com>
2023-01-17 08:57:17 +0800

    Update desk.sh

--- desk.sh
+++ desk.sh
@@ -25,7 +25,7 @@ die () {
 ipv6s1=`nslookup -query=AAAA www.foxset.top | grep 'Address: ' | tail -n1 | awk '{print $NF}' | cut -b 1-20` || die "$ipv6" 
 
 # 前4段与目标设备固定ipv6的后4段（“d1d7:3e3c:3354:de09”）进行拼接
-ipv6s=$ipv6s1"d1d7:3e3c:3354:de09"
+ipv6s=$ipv6s1"a06d:c368:cde8:488"
 
 # 输出ipv6s
 echo $ipv6s
